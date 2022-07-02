287b88713fac9edc3f32a8122b669e8e775b5447
Ryota Higashi <ryo19870130@outlook.jp>
2022-07-02 11:51:11 +0900

    複数ページスクレイピング

--- opt/yodobashi/yodobashi/spiders/desktop.py
+++ opt/yodobashi/yodobashi/spiders/desktop.py
@@ -1,5 +1,6 @@
+from gc import callbacks
 import scrapy
-
+import logging
 
 class DesktopSpider(scrapy.Spider):
     name = 'desktop'
@@ -7,6 +8,7 @@ class DesktopSpider(scrapy.Spider):
     start_urls = ['http://www.yodobashi.com/category/19531/11970/34646/']
 
     def parse(self, response):
+        logging.info(response.url)
         products = response.xpath('//div[contains(@class, "productListTile")]')
         for product in products:
             maker = product.xpath('.//div[contains(@class, "pName")]/p/text()').get()
@@ -18,3 +20,6 @@ class DesktopSpider(scrapy.Spider):
                 'name': name,
                 'price': price,
             }
+        next_page = response.xpath('//a[@class="next"]')
+        if next_page:
+            yield response.follow(url=next_page[0], callback=self.parse)
