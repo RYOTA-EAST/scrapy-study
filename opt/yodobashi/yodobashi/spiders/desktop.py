564c632b3f115a977f5a6937d45444113fe6fc9b
Ryota Higashi <ryo19870130@outlook.jp>
2022-07-03 23:29:14 +0900

    pandas平均値算出

--- opt/yodobashi/yodobashi/spiders/desktop.py
+++ opt/yodobashi/yodobashi/spiders/desktop.py
@@ -9,18 +9,18 @@ class DesktopSpider(scrapy.Spider):
 
     def parse(self, response):
         logging.info(response.request.headers['User-Agent'])
-        # logging.info(response.url)
-        # products = response.xpath('//div[contains(@class, "productListTile")]')
-        # for product in products:
-        #     maker = product.xpath('.//div[contains(@class, "pName")]/p/text()').get()
-        #     name = product.xpath('.//div[contains(@class, "pName")]/p[2]/text()').get()
-        #     price = product.xpath('.//span[@class="productPrice"]/text()').get()
+        logging.info(response.url)
+        products = response.xpath('//div[contains(@class, "productListTile")]')
+        for product in products:
+            maker = product.xpath('.//div[contains(@class, "pName")]/p/text()').get()
+            name = product.xpath('.//div[contains(@class, "pName")]/p[2]/text()').get()
+            price = product.xpath('.//span[@class="productPrice"]/text()').get().strip("￥")
 
-        #     yield {
-        #         'maker': maker,
-        #         'name': name,
-        #         'price': price,
-        #     }
-        # next_page = response.xpath('//a[@class="next"]')
-        # if next_page:
-        #     yield response.follow(url=next_page[0], callback=self.parse)
+            yield {
+                'maker': maker,
+                'name': name,
+                'price': price,
+            }
+        next_page = response.xpath('//a[@class="next"]')
+        if next_page:
+            yield response.follow(url=next_page[0], callback=self.parse)
